23410ec1752154e0eba4fafda51eaa2527249ad7
Hidde Beydals <hello@hidde.co>
2022-01-21 14:45:26 +0100

    Rewrite HelmChartReconciler tests
    Signed-off-by: Hidde Beydals <hello@hidde.co>

--- controllers/legacy_suite_test.go
+++ controllers/legacy_suite_test.go
@@ -30,6 +30,7 @@ import (
 	"helm.sh/helm/v3/pkg/getter"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
